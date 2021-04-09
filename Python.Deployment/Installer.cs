ab1d677600aeaad2dc8eac619e233bdfe7210a16
QUILLAS SAAVEDRA Mario <mquillassaavedra@la-francaise.com>
2021-04-09 10:17:48 +0200

    curl options plus pythonnet standard

--- Python.Deployment/Installer.cs
+++ Python.Deployment/Installer.cs
@@ -322,7 +322,7 @@ namespace Python.Deployment
             if (!Directory.Exists(libDir))
                 Directory.CreateDirectory(libDir);
 
-            RunCommand($"cd {libDir} && curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py");
+            RunCommand($"cd {libDir} && curl -k https://bootstrap.pypa.io/get-pip.py -o get-pip.py");
             RunCommand($"cd {EmbeddedPythonHome} && python.exe Lib\\get-pip.py");
         }
 
